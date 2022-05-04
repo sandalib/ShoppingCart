68ca0ea73af98f190f9752f717560886f204e745
sandalib <2019is015@stu.ucsc.lk>
2022-05-04 09:06:50 +0530

    Cart complete

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,7 +1,27 @@
+import axios from "axios";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Store } from "../Store";
 
 function Product(props) {
     const { product } = props;
+    const { state, dispatch: ctxDispatch } = useContext(Store);
+    const {
+        cart: { cartItems },
+    } = state;
+    const addToCartHandler = async (item) => {
+        const existItem = cartItems.find((x) => x._id === product._id);
+        const quantity = existItem ? existItem.quantity + 1 : 1;
+        const { data } = await axios.get(`/api/products/${item._id}`);
+        if (data.countInStock < quantity) {
+            window.alert('Sorry. Product is out of stock');
+            return;
+        }
+        ctxDispatch({
+            type: 'CART_ADD_ITEM',
+            payload: { ...item, quantity },
+        });
+    };
     return (
         <div className="card">
             <Link to={`/product/${product.slug}`}>
@@ -17,7 +37,15 @@ function Product(props) {
                     <p>
                         <strong>LKR {product.price}</strong>
                     </p>
-                    <button className="btn btn-primary">Add to cart</button>
+                    {product.countInStock === 0 ?
+                        <button type="submit" className="btn btn-light" disabled>
+                            Out of stock
+                        </button>
+                        :
+                        <button type="submit" className="btn btn-primary" onClick={() => addToCartHandler(product)}>
+                            Add to cart
+                        </button>
+                    }
                 </div>
             </div>
         </div>
--- frontend/src/pages/CartScreen.jsx
+++ frontend/src/pages/CartScreen.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import MessageBox from '../components/MessagBox';
@@ -8,6 +9,23 @@ function CartScreen() {
   const {
     cart: { cartItems },
   } = state;
+  const updateCartHandler = async (item, quantity) => {
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    });
+  };
+  const removeItemHandler = (item) => {
+    ctxDispatch({
+      type: 'CART_REMOVE_ITEM',
+      payload: item,
+    });
+  };
   return (
     <div>
       <h1>Your cart</h1>
@@ -35,6 +53,9 @@ function CartScreen() {
                         <button
                           class="btn btn-light"
                           disabled={item.quantity === 1}
+                          onClick={() =>
+                            updateCartHandler(item, item.quantity - 1)
+                          }
                         >
                           <br />
                           <i className="fas fa-minus-circle"></i>
@@ -47,6 +68,9 @@ function CartScreen() {
                       <span>
                         <button
                           class="btn btn-light"
+                          onClick={() =>
+                            updateCartHandler(item, item.quantity + 1)
+                          }
                           disabled={item.quantity === item.countInStock}
                         >
                           <br />
@@ -60,7 +84,11 @@ function CartScreen() {
                     </div>
                     <div className="col-md-2">
                       <br />
-                      <span type="button" class="btn btn-light">
+                      <span
+                        type="button"
+                        class="btn btn-light"
+                        onClick={() => removeItemHandler(item)}
+                      >
                         <i className="fas fa-trash"></i>
                       </span>
                     </div>
